1500df8199e7d66c6a392f6c7d24223a09d7c2bc
riaz <riaz.2012@gmail.com>
2024-02-22 00:41:05 -0800

    sample web service using mux

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # go_web_service
 This is a simple go web service with CI/CD and tests
+
+### Running the docker postgres image (without password)
+
+    docker run -e POSTGRES_HOST_AUTH_METHOD=trust -it -p 5432:5432 -d postgres 
+
+
+### Trouble shooting if the docker is running
+
+    docker ps -a  # this gives the list of containers that ran but failed , copy the container id
+
+    docker logs <container_id> # this allows you to see the logs
+
+    # common steps to stop the container and remove if something is incorrect
+
+    docker stop <container_id>
+
+    docker rm <container_id>
+
--- main_test.go
+++ main_test.go
@@ -2,14 +2,23 @@
 package main_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
+
+	main "github.com/riaz/go_web_service"
 )
 
 var a main.App
 
 func TestMain(m *testing.M) {
+	fmt.Println("Hello")
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
@@ -19,6 +28,150 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 	clearTable()
 	os.Exit(code)
+}
+
+func TestEmptyTables(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+func TestGetNonExistantProduct(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/product/11", nil)
+	response := executeRequest(req)
+
+	// we will check the response for matching the ok status
+	checkResponseCode(t, http.StatusNotFound, response.Code) // since the product doesnt exist
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["error"] != "Product not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Got '%s'", m["error"])
+	}
+
+}
+
+func TestCreateProduct(t *testing.T) {
+
+	clearTable()
+
+	var jsonStr = []byte(`{"name": "test product", "price": 11.22}`)
+
+	req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	// getting a map from string to interface
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test product" {
+		t.Errorf("Expected product name to be 'test product'. Got %v", m["name"])
+	}
+
+	if m["price"] != 11.22 {
+		t.Errorf("Expected product price to be '11.22'. Got '%v'", m["price"])
+	}
+
+	// the id is compared to 1.0 because JSON marshalling converts numbers to floats
+	// when the target is a map[string]interface{}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+	}
+}
+
+// Getting a Product
+func TestGetProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+// Test update product
+func TestUpdateProduct(t *testing.T) {
+	// We will fetch a entry, modify it and persist it.
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	// next we will unmarshall the response
+	var originalProduct map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalProduct)
+
+	// preparing updated fields
+	var jsonStr = []byte(`{"name": "test product - updated name", "price": 11.22}`) // note: the price is still the same
+	req, _ = http.NewRequest("PUT", "/product/1", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+
+	// unmarshalling the response
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	// we will do if the update happened as expected
+	if m["id"] != originalProduct["id"] {
+		t.Errorf("Expected id %v", m["id"])
+	}
+
+	if m["name"] != originalProduct["name"] {
+		t.Errorf("Expected name %v", m["name"])
+	}
+
+	if m["price"] != originalProduct["name"] {
+		t.Errorf("Expected name %v", m["price"])
+	}
+}
+
+func TestDeleteProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	// we added a product and new we want to delete it.
+	req, _ := http.NewRequest("DELETE", "/product/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	// we can verify, that it should not exist anymore
+	req, _ = http.NewRequest("GET", "/product/1", nil)
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+}
+
+func addProducts(count int) {
+	if count < 1 {
+		count = 1
+	}
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO products(name,price) VALUES ($1, $2)", "Product"+strconv.Itoa(i), (i+1.0)*10)
+	}
 }
 
 func ensureTableExists() {
@@ -39,3 +192,16 @@ const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
     price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
     CONSTRAINT products_pkey PRIMARY KEY (id)
 )`
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d got %d", expected, actual)
+	}
+}
